e7af005e454791ea80ddcdfc5615ece5ffbabca1
Sora ichigo <igsr5.dev@gmail.com>
2026-01-20 20:51:45 +0900

    refactor(api): 未使用の graphql/index.ts を削除

--- apps/api/src/graphql/index.ts
+++
@@ -1,4 +0,0 @@
-export { builder, createTestBuilder } from "./builder";
-export type { GraphQLContext } from "./context";
-export { buildSchema, schema } from "./schema";
-export { createApolloServer, createExpressApp } from "./server";
